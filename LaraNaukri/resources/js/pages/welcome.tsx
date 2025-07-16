d8975adf9ff397177f741e22c1d183938ba139d2
tabby8612 <tabishsajwani@hotmail.com>
2025-07-16 14:27:41 +0500

    installed laravel + react, setup SSR, and install and setup tailwind v3 for filament

---
+++ LaraNaukri/resources/js/pages/welcome.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+    return (
+        <div className="bg-red-500 font-montserrat font-extrabold text-heading hover:text-rose-950">
+            <p>Hi</p>
+        </div>
+    );
+}
---
+++ LaraNaukri/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import animatePlugin from 'tailwindcss-animate';
+
+export default {
+    content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.jsx', './resources/**/*.ts', './resources/**/*.tsx'],
+    theme: {
+        extend: {
+            colors: {
+                heading: '#00a264',
+            },
+            fontFamily: {
+                montserrat: 'Montserrat, sans-serif',
+            },
+        },
+    },
+    plugins: [animatePlugin],
+};
